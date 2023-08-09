b8c9f294a982909ef2823f2b984e3a8098d1ec49
vaeryia <moyseychik.v@gmail.com>
2023-08-09 21:19:17 +0300

    Project created

---
+++ HW11/HW11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW11
+//
+//  Created by Valeria Moyseychik on 9.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
