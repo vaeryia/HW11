9fdb688e8a91b66eea72d96b1029d245cfd96f17
vaeryia <moyseychik.v@gmail.com>
2023-08-10 01:43:45 +0300

    1st Part Changes

--- HW11/HW11/ViewController.swift
+++ HW11/HW11/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var progressView: UIProgressView!
+    
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    @IBOutlet var setTimeBtn: UIButton!
+    
+    @IBOutlet var setTimeLbl: UILabel!
+    
+    @IBOutlet var `switch`: UISwitch!
+    
+    @IBOutlet var clearBtn: UIButton!
+    
+ 
+    @IBAction func sliderAction(_ sender: Any) {
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        textField.text = String(slider.value)
+    }
+    
+    
+    @IBAction func datePickerAct(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
